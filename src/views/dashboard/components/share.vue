bb0ff2a30f8703fc36618a53aae8df5134513cbe
4smil3 <939911658@qq.com>
2026-03-23 14:15:52 +0800

    1

--- src/views/dashboard/components/share.vue
+++ src/views/dashboard/components/share.vue
@@ -16,12 +16,13 @@
             <img :src="robotImg" alt="机甲图片" class="robot_img">
             <img src="@/assets/images/share/cover.png" v-if="!isShare" alt="SSR盒子" class="ssr_box">
             <p class="nicknameView" :class="{'created': isShare}">@XXX用户名用户名</p>
+            <img src="@/assets/images/share/qrcode.png" v-if="isShare" alt="二维码" class="qrcode">
           </div>
           <div class="shareBtnBox" v-if="!isShare">
-            <img src="@/assets/images/share/want_share.png" @click="isShare = true" alt="我要分享">
+            <img @click="generateImg" src="@/assets/images/share/want_share.png" alt="我要分享">
           </div>
           <div class="shareBtnBox created" v-else>
-            <img src="@/assets/images/share/save.png" alt="保存图片">
+            <img @click="saveImg" src="@/assets/images/share/save.png" alt="保存图片">
             <img src="@/assets/images/share/share.png" alt="分享">
           </div>
         </div>
@@ -31,6 +32,7 @@
 </template>
 
 <script>
+import html2canvas from 'html2canvas';
 
 export default {
   name: "ShareModal",
@@ -39,6 +41,7 @@ export default {
       visible: false, //分享弹窗是否显示
       robotImg: require('@/assets/images/share/robot.png'), //机甲图片
       isShare: false, //是否分享
+      base64Img: '', //分享图片base64
     };
   },
   watch: {
@@ -52,6 +55,96 @@ export default {
     closeShare() {
       this.visible = false;
       this.isShare = false;
+    },
+    //生成图片
+    async generateImg() {
+      this.isShare = true;
+      await this.$nextTick();
+      await new Promise((resolve) => requestAnimationFrame(() => requestAnimationFrame(resolve)));
+      if (document.fonts && document.fonts.ready) {
+        await document.fonts.ready.catch(() => undefined);
+      }
+      const shareBox = document.getElementById('shareBox');
+      const exportWidth = 670;
+      const exportHeight = 950;
+      const canvas = await html2canvas(shareBox, {
+        backgroundColor: null,
+        useCORS: true,
+        scale: 1,
+        width: exportWidth,
+        height: exportHeight,
+        windowWidth: exportWidth,
+        windowHeight: exportHeight,
+        onclone: (clonedDoc) => {
+          const clonedShareBox = clonedDoc.getElementById('shareBox');
+          if (!clonedShareBox) return;
+
+          const container = clonedShareBox.closest('.shareBox_container');
+          if (container) {
+            container.style.transform = 'none';
+            container.style.transformOrigin = 'top left';
+            container.style.width = `${exportWidth}px`;
+            container.style.height = `${exportHeight}px`;
+          }
+
+          const stage = clonedShareBox.closest('.shareBox_stage');
+          if (stage) {
+            stage.style.width = `${exportWidth}px`;
+            stage.style.height = `${exportHeight}px`;
+            stage.style.overflow = 'visible';
+          }
+
+          clonedShareBox.style.width = `${exportWidth}px`;
+          clonedShareBox.style.height = `${exportHeight}px`;
+        },
+      });
+      this.base64Img = canvas.toDataURL('image/png');
+    },
+    //保存图片
+    saveImg() {
+      // 检查是否在B站App中
+      const { inBiliApp, isSupport, callNative } = window.biliBridge;
+      if (inBiliApp) {
+        isSupport("ability.saveImageToPhotosAlbum").then((support) => {
+          if (support) {
+            callNative({
+              method: "ability.saveImageToPhotosAlbum",
+              data: {
+                base64Data: this.base64Img, // base64 字符串，上限 2M，filePath 与 base64Data 必填其一
+                hintMsg: "需要保存图片到相册，以便您可以分享给朋友", // GR 要求申请权限时必须要给予相应的场景说明。
+              },
+              callback: (info) => {
+                console.log("保存图片结果:", info);
+                // 保存成功后关闭弹窗
+                if (info.code === 0) {
+                  this.$message.success("保存成功");
+                } else {
+                  this.$message.error(info.errMsg || "保存失败");
+                }
+                this.closeShare();
+              },
+            });
+          } else {
+            // not support
+            this.downloadImage();
+          }
+        });
+      } else {
+        this.downloadImage();
+      }
+    },
+    //下载图片 
+    downloadImage() {
+      const link = document.createElement("a");
+      link.href = this.base64Img;
+      link.download = "share_image.png";
+
+      // 触发下载
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      this.$message.success("保存成功");
+      this.closeShare();
     },
   },
 };
@@ -134,6 +227,13 @@ export default {
     top: 0;
     width: 100%;
     height: 100%;
+  }
+  .qrcode {
+    position: absolute;
+    left: 26px;
+    bottom: 135px;
+    width: 134px;
+    height: 134px;
   }
   .nicknameView {
     position: absolute;
