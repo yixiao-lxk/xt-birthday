aab62ef793e1040d86e82b8c47993011c54cfa6f
4smil3 <939911658@qq.com>
2026-04-10 18:01:50 +0800

    组件

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -150,6 +150,9 @@ export default {
       const {
         config: { activity_id },
       } = this;
+      if(this.components[index].group_status == 0) {
+        return;
+      }
       if(this.components[index].remain_refresh_count == 0) {
         this.$message.error(`暂无刷新次数，请先观看第${index+1}组视频1分钟获取刷新次数`);
         return;
